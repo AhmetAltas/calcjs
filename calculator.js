03443f27d9a99856009e87234184aa59a82444ae
AHMETA\ahmet <99040436@mydavinci.nl>
2017-02-21 14:15:45 +0100

    af

--- calculator.js
+++ calculator.js
@@ -1,69 +1,79 @@
-var round = function(text, pos) {
-    function setCharAt(str,index,chr) {
-       if(index > str.length-1) return str;
-        return str.substr(0,index) + chr + str.substr(index+1);
-    }
-    if (text.length > pos) {
-        for (i=1;i<text.length;i++){
-            if (text[i] === ".") {
-                i = text.length;
-                if (parseFloat(text[pos],10) >= 5) {
-                    text = (setCharAt(text,pos-1,((parseFloat(text[pos-1],10))+1).toString(10))).substr(0,pos);
-                } else {
-                    text = text.substr(0,pos);
-                }
-            } 
-        }
-    }
-    return text;
-};
-var $ = jQuery.noConflict();
-$(function() {
-    var testNumLength = function(number) {
-        if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
-            if (number.length > 15) {
-                number = "";
-                totaldiv.text("Err");
-            }
-        } 
-    };
-    var number = "";
-    var newnumber = "";
-    var operator = "";
-    var totaldiv = $("#total");
-    totaldiv.text("0");
-    $("#numbers > a").not("#clear,#clearall").click(function(){
-		number += $(this).text();
-		totaldiv.text(number);
-		testNumLength(number);
-    });
-    $("#operators > a").not("#equals").click(function(){
-		operator = $(this).text();
-		newnumber = number;
-		number = "";
-		totaldiv.text("0");
-    });
-    $("#clear,#clearall").click(function(){
-		number = "";
-		totaldiv.text("0");
-		if ($(this).attr("id") === "clearall") {
-			newnumber = "";
-		}
-    });
-    $("#equals").click(function(){
-		if (operator === "+"){
-			number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
-		} else if (operator === "-"){
-			number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
-		} else if (operator === "/"){
-			number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
-		} else if (operator === "*"){
-			number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
-		}
-		totaldiv.text(number);
-		testNumLength(number);
-		number = "";
-		newnumber = "";
-    });
-}); 
+var operator = "";
+
+function num(val) {
+	document.getElementById('input').value += val;
+}
+
+function btnPlus() {
+	getNum();
+
+	document.getElementById('input').value = "";
+
+	operator = "1";
+}
+
+function btnMinus() {
+	getNum();
+
+	document.getElementById('input').value = "";
+
+	operator = "2";
+}
+
+function btnDivide() {
+	getNum();
+
+	document.getElementById('input').value = "";
+
+	operator = "3";
+}
+
+function btnTimes() {
+	getNum();
+
+	document.getElementById('input').value = "";
+
+	operator = "4";
+}
+
+
+function btnEquals() {
+	getNum2();
+
+	var ans1 = document.getElementById('getal1').innerText;
+	var ans2 = document.getElementById('getal2').innerText;
+
+	if (operator == "1") {
+		var ans = +ans1 + +ans2;
+		ans = ans.toFixed(1);
+	}
+
+	if (operator == "2") {
+		var ans = +ans1 - +ans2;
+		ans = ans.toFixed(1);
+	}
+
+	if (operator == "3") {
+		var ans = +ans1 / +ans2;
+	}
+
+	if (operator == "4") {
+		var ans = +ans1 * +ans2;
+	}
+
+	document.getElementById('input').value = ans;
+}
+
+function getNum() {
+   var get1 = document.getElementById("input").value;
+   document.getElementById("getal1").innerText = get1;
+}
+
+function getNum2() {
+	var get2 = document.getElementById("input").value;
+	document.getElementById("getal2").innerText = get2;
+}
+
+function btnClear() {
+	document.getElementById('input').value = "";
+}
